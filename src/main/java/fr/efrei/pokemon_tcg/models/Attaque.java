f1d46701ec17e45218b45f100d665d2b4db53c12
Lucas TEAR <lucas.tear+1@efrei.net>
2025-01-30 14:19:19 +0100

    Ajout de la classe Attaque pour gérer les attaques des Pokémon avec génération aléatoire des dégâts et nettoyage de la classe Card

---
+++ src/main/java/fr/efrei/pokemon_tcg/models/Attaque.java
@@ -0,0 +1,45 @@
+package fr.efrei.pokemon_tcg.models;
+
+import java.util.Random;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attaque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+
+    private String nom;
+
+    private int degats;
+
+    public Attaque() {
+        this.degats = genererDegatsAleatoires();
+    }
+
+    private int genererDegatsAleatoires() {
+        Random random = new Random();
+        return 50 + random.nextInt(151);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getDegats() {
+        return degats;
+    }
+}
--- src/main/java/fr/efrei/pokemon_tcg/models/Pokemon.java
+++ src/main/java/fr/efrei/pokemon_tcg/models/Pokemon.java
@@ -1,7 +1,5 @@
 package fr.efrei.pokemon_tcg.models;
 
-import java.util.Random;
-
 import fr.efrei.pokemon_tcg.constants.TypePokemon;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -22,17 +20,6 @@ public class Pokemon {
     @Enumerated(EnumType.STRING)
     private TypePokemon type;
 
-    private String attack1;
-    private String attack2;
-    private int attack1Power;
-    private int attack2Power;
-
-    public Pokemon() {
-        Random random = new Random();
-        this.attack1Power = random.nextInt(200) + 1;
-        this.attack2Power = random.nextInt(200) + 1;
-    }
-
     public String getNom() {
         return nom;
     }
@@ -47,38 +34,6 @@ public class Pokemon {
 
     public void setType(TypePokemon type) {
         this.type = type;
-    }
-
-    public String getAttack1() {
-        return attack1;
-    }
-
-    public void setAttack1(String attack1) {
-        this.attack1 = attack1;
-    }
-
-    public String getAttack2() {
-        return attack2;
-    }
-
-    public void setAttack2(String attack2) {
-        this.attack2 = attack2;
-    }
-
-    public int getAttack1Power() {
-        return attack1Power;
-    }
-
-    public void setAttack1Power(int attack1Power) {
-        this.attack1Power = attack1Power;
-    }
-
-    public int getAttack2Power() {
-        return attack2Power;
-    }
-
-    public void setAttack2Power(int attack2Power) {
-        this.attack2Power = attack2Power;
     }
 
     public String getUuid() {
