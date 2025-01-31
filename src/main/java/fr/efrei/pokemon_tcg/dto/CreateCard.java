21eda06c106b992407e30e4bdca226fff0074800
Lucas TEAR <lucas.tear+1@efrei.net>
2025-01-31 15:57:56 +0100

    Suppression des getters et setters inutilisés dans CreateCard et ajout de nouvelles lignes à la fin des fichiers

--- src/main/java/fr/efrei/pokemon_tcg/dto/CreateCard.java
+++ src/main/java/fr/efrei/pokemon_tcg/dto/CreateCard.java
@@ -1,13 +1,4 @@
 package fr.efrei.pokemon_tcg.dto;
 
 public class CreateCard {
-    private String pokemonUuid;
-
-    public String getPokemonUuid() {
-        return pokemonUuid;
-    }
-
-    public void setPokemonUuid(String pokemonUuid) {
-        this.pokemonUuid = pokemonUuid;
-    }
 }
